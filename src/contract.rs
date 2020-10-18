8c747e180a10638d8127e45f481fd59f35b4402f
Simon Warta <simon@warta.it>
2020-10-18 22:43:28 +0200

    Update cosmwasm to 0.11

--- src/contract.rs
+++ src/contract.rs
@@ -1,5 +1,6 @@
 use cosmwasm_std::{
-    to_binary, Api, Binary, Env, Extern, HandleResponse, InitResponse, Querier, StdResult, Storage,
+    to_binary, Api, Binary, Env, Extern, HandleResponse, InitResponse, MessageInfo, Querier,
+    StdResult, Storage,
 };
 use drand_verify::{g1_from_fixed, verify};
 
@@ -17,6 +18,7 @@ const PK_LEO_MAINNET: [u8; 48] = [
 pub fn init<S: Storage, A: Api, Q: Querier>(
     deps: &mut Extern<S, A, Q>,
     _env: Env,
+    _info: MessageInfo,
     msg: InitMsg,
 ) -> StdResult<InitResponse> {
     let state = State { round: msg.round };
@@ -29,6 +31,7 @@ pub fn init<S: Storage, A: Api, Q: Querier>(
 pub fn handle<S: Storage, A: Api, Q: Querier>(
     _deps: &mut Extern<S, A, Q>,
     _env: Env,
+    _info: MessageInfo,
     msg: HandleMsg,
 ) -> StdResult<HandleResponse> {
     match msg {
@@ -63,6 +66,7 @@ pub fn try_verify(
 
 pub fn query<S: Storage, A: Api, Q: Querier>(
     deps: &Extern<S, A, Q>,
+    _env: Env,
     msg: QueryMsg,
 ) -> StdResult<Binary> {
     match msg {
@@ -80,43 +84,40 @@ fn query_latest<S: Storage, A: Api, Q: Querier>(
 #[cfg(test)]
 mod tests {
     use super::*;
-    use cosmwasm_std::testing::{mock_dependencies, mock_env};
+    use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
     use cosmwasm_std::{coins, from_binary};
 
     #[test]
     fn proper_initialization() {
-        let mut deps = mock_dependencies(20, &[]);
+        let mut deps = mock_dependencies(&[]);
 
+        let info = mock_info("creator", &coins(1000, "earth"));
         let msg = InitMsg { round: 17 };
-        let env = mock_env("creator", &coins(1000, "earth"));
 
-        // we can just call .unwrap() to assert this was a success
-        let res = init(&mut deps, env, msg).unwrap();
+        let res = init(&mut deps, mock_env(), info, msg).unwrap();
         assert_eq!(res.messages.len(), 0);
 
-        // it worked, let's query the state
-        let res = query(&deps, QueryMsg::Latest {}).unwrap();
+        let res = query(&deps, mock_env(), QueryMsg::Latest {}).unwrap();
         let value: LatestResponse = from_binary(&res).unwrap();
         assert_eq!(value.round, 17);
     }
 
     #[test]
     fn verify() {
-        let mut deps = mock_dependencies(20, &coins(2, "token"));
+        let mut deps = mock_dependencies(&coins(2, "token"));
 
+        let info = mock_info("creator", &coins(2, "token"));
         let msg = InitMsg { round: 17 };
-        let env = mock_env("creator", &coins(2, "token"));
-        let _res = init(&mut deps, env, msg).unwrap();
+        let _res = init(&mut deps, mock_env(), info, msg).unwrap();
 
-        // beneficiary can release it
-        let env = mock_env("anyone", &coins(2, "token"));
+        let info = mock_info("anyone", &coins(2, "token"));
         let msg = HandleMsg::Verify {
             // curl -sS https://drand.cloudflare.com/public/72785
             round: 72785,
             previous_signature: hex::decode("a609e19a03c2fcc559e8dae14900aaefe517cb55c840f6e69bc8e4f66c8d18e8a609685d9917efbfb0c37f058c2de88f13d297c7e19e0ab24813079efe57a182554ff054c7638153f9b26a60e7111f71a0ff63d9571704905d3ca6df0b031747").unwrap().into(),
             signature: hex::decode("82f5d3d2de4db19d40a6980e8aa37842a0e55d1df06bd68bddc8d60002e8e959eb9cfa368b3c1b77d18f02a54fe047b80f0989315f83b12a74fd8679c4f12aae86eaf6ab5690b34f1fddd50ee3cc6f6cdf59e95526d5a5d82aaa84fa6f181e42").unwrap().into(),
         };
-        let res = handle(&mut deps, env, msg).unwrap();
+        let res = handle(&mut deps, mock_env(), info, msg).unwrap();
         assert_eq!(res.data.unwrap().as_slice(), [0x01]);
     }
 }
--- tests/integration.rs
+++ tests/integration.rs
@@ -19,7 +19,7 @@
 
 use cosmwasm_std::{coins, from_binary, HandleResponse, InitResponse};
 use cosmwasm_vm::testing::{
-    handle, init, mock_env, mock_instance, mock_instance_with_gas_limit, query,
+    handle, init, mock_env, mock_info, mock_instance, mock_instance_with_gas_limit, query,
 };
 
 use rand::msg::{HandleMsg, InitMsg, LatestResponse, QueryMsg};
@@ -32,14 +32,14 @@ fn proper_initialization() {
     let mut deps = mock_instance(WASM, &[]);
 
     let msg = InitMsg { round: 17 };
-    let env = mock_env("creator", &coins(1000, "earth"));
+    let info = mock_info("creator", &coins(1000, "earth"));
 
     // we can just call .unwrap() to assert this was a success
-    let res: InitResponse = init(&mut deps, env, msg).unwrap();
+    let res: InitResponse = init(&mut deps, mock_env(), info, msg).unwrap();
     assert_eq!(res.messages.len(), 0);
 
     // it worked, let's query the state
-    let res = query(&mut deps, QueryMsg::Latest {}).unwrap();
+    let res = query(&mut deps, mock_env(), QueryMsg::Latest {}).unwrap();
     let value: LatestResponse = from_binary(&res).unwrap();
     assert_eq!(value.round, 17);
 }
@@ -49,19 +49,19 @@ fn verify_valid() {
     let mut deps = mock_instance_with_gas_limit(WASM, 1_000_000_000);
 
     let msg = InitMsg { round: 17 };
-    let env = mock_env("creator", &[]);
-    let _res: InitResponse = init(&mut deps, env, msg).unwrap();
+    let info = mock_info("creator", &[]);
+    let _res: InitResponse = init(&mut deps, mock_env(), info, msg).unwrap();
 
     let gas_before = deps.get_gas_left();
 
-    let env = mock_env("anyone", &[]);
+    let info = mock_info("anyone", &[]);
     let msg = HandleMsg::Verify {
         // curl -sS https://drand.cloudflare.com/public/72785
         round: 72785,
         previous_signature: hex::decode("a609e19a03c2fcc559e8dae14900aaefe517cb55c840f6e69bc8e4f66c8d18e8a609685d9917efbfb0c37f058c2de88f13d297c7e19e0ab24813079efe57a182554ff054c7638153f9b26a60e7111f71a0ff63d9571704905d3ca6df0b031747").unwrap().into(),
         signature: hex::decode("82f5d3d2de4db19d40a6980e8aa37842a0e55d1df06bd68bddc8d60002e8e959eb9cfa368b3c1b77d18f02a54fe047b80f0989315f83b12a74fd8679c4f12aae86eaf6ab5690b34f1fddd50ee3cc6f6cdf59e95526d5a5d82aaa84fa6f181e42").unwrap().into(),
     };
-    let res: HandleResponse = handle(&mut deps, env, msg).unwrap();
+    let res: HandleResponse = handle(&mut deps, mock_env(), info, msg).unwrap();
     assert_eq!(res.data.unwrap().as_slice(), [0x01]);
 
     let gas_used = gas_before - deps.get_gas_left();
@@ -73,19 +73,19 @@ fn verify_invalid() {
     let mut deps = mock_instance_with_gas_limit(WASM, 1_000_000_000);
 
     let msg = InitMsg { round: 17 };
-    let env = mock_env("creator", &[]);
-    let _res: InitResponse = init(&mut deps, env, msg).unwrap();
+    let info = mock_info("creator", &[]);
+    let _res: InitResponse = init(&mut deps, mock_env(), info, msg).unwrap();
 
     let gas_before = deps.get_gas_left();
 
-    let env = mock_env("anyone", &[]);
+    let info = mock_info("anyone", &[]);
     let msg = HandleMsg::Verify {
         // curl -sS https://drand.cloudflare.com/public/72785
         round: 42,
         previous_signature: hex::decode("a609e19a03c2fcc559e8dae14900aaefe517cb55c840f6e69bc8e4f66c8d18e8a609685d9917efbfb0c37f058c2de88f13d297c7e19e0ab24813079efe57a182554ff054c7638153f9b26a60e7111f71a0ff63d9571704905d3ca6df0b031747").unwrap().into(),
         signature: hex::decode("82f5d3d2de4db19d40a6980e8aa37842a0e55d1df06bd68bddc8d60002e8e959eb9cfa368b3c1b77d18f02a54fe047b80f0989315f83b12a74fd8679c4f12aae86eaf6ab5690b34f1fddd50ee3cc6f6cdf59e95526d5a5d82aaa84fa6f181e42").unwrap().into(),
     };
-    let res: HandleResponse = handle(&mut deps, env, msg).unwrap();
+    let res: HandleResponse = handle(&mut deps, mock_env(), info, msg).unwrap();
     assert_eq!(res.data.unwrap().as_slice(), [0x00]);
 
     let gas_used = gas_before - deps.get_gas_left();
